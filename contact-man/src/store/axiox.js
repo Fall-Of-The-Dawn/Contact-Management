61ae06eefc9e5d13138b92c45d6610623133f67c
kishoor <kishoor0920@gmail.com>
2026-01-03 17:32:38 +0530

    final update

--- contact-man/src/store/axiox.js
+++ contact-man/src/store/axiox.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ContactAxios = axios.create({
-    baseURL: `${import.meta.env.Backend_Port}/api/users`,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/users`,
     withCredentials: true
 
 })
