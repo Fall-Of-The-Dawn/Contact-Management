33b668f2ba66d972b5adc505e6b80f882a0082df
kishoor <kishoor0920@gmail.com>
2026-01-03 17:17:32 +0530

    Final Update

--- contact-man/src/store/axiox.js
+++ contact-man/src/store/axiox.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ContactAxios = axios.create({
-    baseURL: "http://localhost:3000/api/users",
+    baseURL: `${import.meta.env.Backend_Port}/api/users`,
     withCredentials: true
 
 })
--- contact-man2/api/index.js
+++ contact-man2/api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import {connectDB} from './lib/db.lib.js';
-import usersRoute from './routes/users.route.js'
+import {connectDB} from '../src/lib/db.lib.js';
+import usersRoute from '../src/routes/users.route.js'
 import cors from 'cors'
 
 const app = express();
@@ -9,12 +9,7 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors(
-    {
-    origin: "https://contact-management-sepia-omega.vercel.app/",
-    credentials: true
-    }
-))
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/users', usersRoute)
@@ -28,4 +23,4 @@ try{
 }catch(err){
     console.log(err.message)
 }
-app.listen(Port, () => console.log(`Server running on port ${Port}`)); 
+export default app;
