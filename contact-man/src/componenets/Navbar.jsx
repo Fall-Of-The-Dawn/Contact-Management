e8acaf44dc1cc07a494b59ba8378dbb07d6ac360
kishoor <kishoor0920@gmail.com>
2026-01-04 12:08:06 +0530

    Final Update

--- contact-man/src/componenets/Navbar.jsx
+++ contact-man/src/componenets/Navbar.jsx
@@ -4,12 +4,18 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm bg-gradient-to-r from-cyan-700 to-blue-900">
         <div className="navbar-start">
-        <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
             </div>
-            
-        </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/findContacts">Find Contacts</Link></li>
+              <li><Link to="/contact">Add Contact</Link></li>
+            </ul>
+          </div>
         <a className="btn btn-ghost text-xl">Uravadi</a>
         </div>
         <div className="navbar-center hidden lg:flex">
